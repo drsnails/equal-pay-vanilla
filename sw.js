e7dfb497bf23c9fc8208147298b0e237faca145a
Stav Yaar Bar <anistu@gmail.com>
2023-10-05 12:33:26 +0300

    some styling and pwa

--- sw.js
+++ sw.js
@@ -1,52 +1,70 @@
-
 console.log('Service Worker Registered!');
-// console.log('self!:', self)
 
-// This function build an array of urls,
-// fetch them, and store the responses in the cache,
-// example: key: 'main.js' value: 'alert(3)'
+let CACHE_NAME = 'equal-pay-v2';  // Update the version when you want to push new content
+
+const urlsToCache = [
+    'https://drsnails.github.io/equal-pay-vanilla/',
+    'https://drsnails.github.io/equal-pay-vanilla/index.html',
+    'https://drsnails.github.io/equal-pay-vanilla/manifest.json',
+    'https://drsnails.github.io/equal-pay-vanilla/sw.js',
+    'https://drsnails.github.io/equal-pay-vanilla/main.js',
+    'https://drsnails.github.io/equal-pay-vanilla/services/utils.js',
+    'https://drsnails.github.io/equal-pay-vanilla/services/storage.service.js',
+    'https://drsnails.github.io/equal-pay-vanilla/services/transaction.service.js',
+    'https://drsnails.github.io/equal-pay-vanilla/css/style.css',
+    'https://drsnails.github.io/equal-pay-vanilla/right-arrow.svg',
+    'https://drsnails.github.io/equal-pay-vanilla/img/logo-152.png',
+]
+
 self.addEventListener('install', event => {
-
     console.log('Installing service worker...');
-
-
-    const urlsToCache = [
-        'https://drsnails.github.io/equal-pay-vanilla/',
-        'https://drsnails.github.io/equal-pay-vanilla/index.html',
-        'https://drsnails.github.io/equal-pay-vanilla/manifest.json',
-        'https://drsnails.github.io/equal-pay-vanilla/sw.js',
-        'https://drsnails.github.io/equal-pay-vanilla/main.js',
-        'https://drsnails.github.io/equal-pay-vanilla/services/utils.js',
-        'https://drsnails.github.io/equal-pay-vanilla/services/storage.service.js',
-        'https://drsnails.github.io/equal-pay-vanilla/services/transaction.service.js',
-        'https://drsnails.github.io/equal-pay-vanilla/css/style.css',
-        'https://drsnails.github.io/equal-pay-vanilla/right-arrow.svg',
-        'https://drsnails.github.io/equal-pay-vanilla/img/logo-152.png',
-    ]
     event.waitUntil(
-        caches.open('my-cache1').then(cache => {
-            return cache.addAll(urlsToCache)
-        })
-    )
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                // console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
 
-})
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (CACHE_NAME !== cacheName && cacheName.startsWith('equal-pay')) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
 
 self.addEventListener('fetch', event => {
-    console.log('Fetch of: ', event.request.url)
+    if (event.request.url.startsWith('chrome-extension')) {
+        return; // Don't handle chrome-extension:// URLs
+    }
 
     event.respondWith(
-        // the response is resolved to null if there is no match 
         caches.match(event.request)
             .then(response => {
-                var res = response
-
-                if (!res) {
-                    console.log(event.request.url, '- NOT IN CACHE, FETCHED FROM NETWORK!')
-                    res = fetch(event.request)
-                } else {
-                    console.log(event.request.url, 'FOUND IN CACHE')
+                if (response) {
+                    console.log(event.request.url, 'FOUND IN CACHE');
+                    return response;
                 }
-                return res
+                console.log(event.request.url, '- NOT IN CACHE, FETCHED FROM NETWORK!');
+                return fetch(event.request).then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    let responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseToCache);
+                        });
+                    return response;
+                });
             })
-    )
-})
+    );
+});
